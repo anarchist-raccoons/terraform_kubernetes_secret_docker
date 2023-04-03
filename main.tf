b6df211da3991563fa27c44dea91baa6ac904bb6
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2023-04-03 13:48:49 +0100

    branch this next time :|

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 provider "kubernetes" {
 #  load_config_file = false
-  host = var.host
-  username = var.username
-  password = var.password
-  client_certificate = var.client_certificate
-  client_key = var.client_key
-  cluster_ca_certificate = var.cluster_ca_certificate
+  host = "${var.host}"
+  username = "${var.username}"
+  password = "${var.password}"
+  client_certificate = "${var.client_certificate}"
+  client_key = "${var.client_key}"
+  cluster_ca_certificate = "${var.cluster_ca_certificate}"
 }
 
 # Secret for docker login
 resource "kubernetes_secret" "default" {
-  depends_on = [null_resource.docker]
+  depends_on = ["null_resource.docker"]
   
   metadata {
-    name = var.kubernetes_secret
+    name = "${var.kubernetes_secret}"
   }
   data = {
-    ".dockerconfigjson" = file("${path.cwd}/config.json")
+    ".dockerconfigjson" = "${file("${path.cwd}/config.json")}"
   }
   type = "kubernetes.io/dockerconfigjson"
 }
